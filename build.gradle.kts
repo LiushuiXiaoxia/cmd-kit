b1f7e294dde256fd9008c6cc5269567fee7b16d8
xiaqiulei <xiaqiulei@bilibili.com>
2025-05-15 17:03:04 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "2.1.20"
+}
+
+group = "cn.mycommons"
+version = project.properties["LIB_VERSION"].toString()
+
+repositories {
+    maven("https://maven.aliyun.com/repository/public")
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/kotlin/cn/mycommons/processkit/ProcessKit.kt
@@ -0,0 +1,9 @@
+package cn.mycommons.cn.mycommons.processkit
+
+object ProcessKit {
+
+    @JvmStatic
+    fun run() {
+
+    }
+}
