ed8464372731ae3d2d9133343501ca558a357600
xiaqiulei <xiaqiulei@bilibili.com>
2025-05-19 12:33:25 +0800

    update publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-buildscript{
+buildscript {
     repositories {
         maven("https://maven.aliyun.com/repository/public")
         mavenCentral()
@@ -7,9 +7,11 @@ buildscript{
 
 plugins {
     kotlin("jvm") version "2.1.20"
+    `java-library`
+    `maven-publish`
 }
 
-group = "cn.mycommons"
+group = "dev.LiushuiXiaoxia"
 version = project.properties["LIB_VERSION"].toString()
 
 repositories {
@@ -27,4 +29,45 @@ tasks.test {
 
 kotlin {
     jvmToolchain(11)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                name.set("process-kit") // 项目名称
+                description.set("kotlin/java call process kit")
+                url.set("https://github.com/yourusername/yourproject")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("LiushuiXiaoxia")
+                        name.set("LiushuiXiaoxia")
+                        url.set("https://github.com/LiushuiXiaoxia")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/LiushuiXiaoxia/process-kit.git")
+                    developerConnection.set("scm:git:ssh://github.com/LiushuiXiaoxia/process-kit.git")
+                    url.set("https://github.com/LiushuiXiaoxia/process-kit")
+                }
+            }
+        }
+    }
 }
