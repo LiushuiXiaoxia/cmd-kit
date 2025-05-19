2b9746065682617f7291421dc78ff10caecd14b2
xiaqiulei <xiaqiulei@bilibili.com>
2025-05-19 11:55:03 +0800

    test example

--- src/test/java/cn/mycommons/processkit/ProcessTest.kt
+++ src/test/java/cn/mycommons/processkit/ProcessTest.kt
@@ -1,6 +1,7 @@
 package cn.mycommons.processkit
 
 import org.testng.annotations.Test
+import java.io.File
 
 class ProcessTest {
 
@@ -10,5 +11,18 @@ class ProcessTest {
         val ret = p.asProcessResult()
         println("ret = $ret")
         println("pid = ${p.reflectPid()}")
+    }
+
+    @Test
+    fun testExample() {
+        val tmp = File("tmp", "t-${System.currentTimeMillis()}").canonicalFile
+        println("tmp = $tmp")
+        ProcessKit.call("mkdir -p $tmp").check("make dir fail")
+        ProcessKit.call("git clone git@github.com:LiushuiXiaoxia/process-kit.git $tmp").check("git check failed")
+        ProcessKit.run("cd $tmp && git status && git log -5")
+        ProcessKit.run("cd $tmp && ./gradlew clean assemble")
+        ProcessKit.run("rm -rf $tmp")
+
+        println("tmp = ${tmp.exists()}")
     }
 }
